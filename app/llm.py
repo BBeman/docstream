15e17d3b87bb917816babb8572f8263aa7b9fc95
BBeman <barbodbemanzadeh13@gmail.com>
2026-05-28 17:25:49 +0100

    async anthropic client wrapper

---
+++ app/llm.py
@@ -0,0 +1,21 @@
+from anthropic import AsyncAnthropic
+from app.config import settings
+
+client = AsyncAnthropic(
+    api_key= settings.anthropic_api_key
+)
+
+
+async def summarise(text : str, max_words: int = 100) -> str:
+    prompt = f"summarize in a total word count of: {max_words}:\n\n{text}. make sure to sound cute when saying it"
+    message = await client.messages.create(
+        max_tokens=1024,
+        messages=[
+            {
+            "role" : "user",
+            "content" : prompt
+        }
+        ],
+        model= settings.model_name
+    )
+    return message.content[0].text
